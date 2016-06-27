b93a0c77d1779985a17935aa760f152841c2757f
davasens <davidasensio81@gmail.com>
2016-06-27 16:49:41 +0200

    app.js to server.js

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ io.sockets.on('connection', function(socket) {
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 server.listen(server_port, server_ip_address, function() {
-	console.log('Listening on port: ' + port);
+	console.log('Listening on port: ' + server_port);
 });
